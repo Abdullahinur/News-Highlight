9b463f4a6ed24f28e76e5553a199e24db28076fe
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-15 17:48:47 +0300

    add test class for articles and sources both fail

--- app/main/views.py
+++ app/main/views.py
@@ -5,4 +5,11 @@ from ..requests import get_sources, get_articles
 
 @main.route('/')
 def index():
-    return render_template('')
+    general_list = get_sources('us', 'general')
+    business_list = get_sources('us', 'business')
+    technology_list = get_sources('us', 'technology')
+    sports_list = get_sources('us', 'sports')
+    science_list = get_sources('us', 'science')
+    enterntainment_list = get_sources('us', 'enterntainment')
+    health_list = get_sources('us', 'health')
+    return render_template('index.html', general=general_list, business=business_list, technology=technology_list, sports=sports_list, scince=science_list, entertainment=enterntainment_list, health=health_list)
